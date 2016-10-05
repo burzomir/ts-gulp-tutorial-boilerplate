d670783d331fc793c2b2acd3c78f7b1f241301d2
mklobukowski <neomic66@gmail.com>
2016-10-05 13:24:46 +0200

    watchify

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
+var gutil = require("gulp-util");
 var browserify = require('browserify');
+var watchify = require("watchify");
 var source = require('vinyl-source-stream');
 var tsify = require('tsify');
 var sourcemaps = require('gulp-sourcemaps');
@@ -8,28 +10,37 @@ var paths = {
     pages: ['src/*.html']
 };
 
-gulp.task('copyHtml', function () {
-    return gulp.src(paths.pages)
-        .pipe(gulp.dest('dist'));
-});
-
-gulp.task('default', ['copyHtml'], function () {
-    return browserify({
+var watchedBrowserify = watchify(browserify({
         basedir: '.',
         debug: true,
         entries: ['src/main.ts'],
         cache: {},
         packageCache: {}
     })
-    .plugin(tsify)
-    .transform('babelify', {
-        presets: ['es2015'],
-        extensions: ['.ts']
-    })
-    .bundle()
-    .pipe(source('bundle.js'))
-    .pipe(buffer())
-    .pipe(sourcemaps.init({loadMaps: true}))
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('dist'));
+    .plugin(tsify));
+
+function bundle() {
+    return watchedBrowserify
+        .transform('babelify', {
+            presets: ['es2015'],
+            extensions: ['.ts']
+        })
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({
+            loadMaps: true
+        }))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('dist'));
+}
+
+gulp.task('copyHtml', function () {
+    return gulp.src(paths.pages)
+        .pipe(gulp.dest('dist'));
 });
+
+gulp.task('default', ['copyHtml'], bundle);
+
+watchedBrowserify.on('update', bundle);
+watchedBrowserify.on('log', gutil.log);
